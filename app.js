5459f9ada1f656cdd5b985e51bce0602d02417b7
AbhinavGor <abhinav20016@gmail.com>
2021-04-10 15:22:25 +0530

    Added passport config for google oauth

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const User = require('./models/User')
 const PORT = process.env.PORT;
 const app = express();
 
+
+//Passport config
+require('./middleware/passport')(passport)
+
 //Connect to mongoDB
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
